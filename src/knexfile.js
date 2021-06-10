822df93b1caae14821794cf2b1f65e16a84cb2a3
analindiner <almm.analindiner@gmail.com>
2021-06-09 22:42:34 -0300

    refatoracao e configuracao do banco de dados

---
+++ src/knexfile.js
@@ -0,0 +1,24 @@
+// Update with your config settings.
+import path from 'path'
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      host: '127.0.0.1',
+      user: 'root',
+      password: '',
+      database: "netflix",
+      port: 3306
+
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: path.resolve(__dirname, 'database', 'migrations')
+    },
+    seeds: {
+      directory: path.resolve(__dirname, 'database', 'seeds')
+    }
+  }
+};
